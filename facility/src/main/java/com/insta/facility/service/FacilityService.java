16036fa38ff48f64e3262ef10dff15e955b13851
Tom-Renaud <tomrenaud9@free.Fr>
2020-10-29 14:13:28 +0100

    Commit facility service v1

--- facility/src/main/java/com/insta/facility/service/FacilityService.java
+++ facility/src/main/java/com/insta/facility/service/FacilityService.java
@@ -1,4 +1,12 @@
 package com.insta.facility.service;
 
+import com.insta.facility.repository.FacilityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FacilityService {
+    @Autowired
+    private FacilityRepository facilityRepository;
+
 }
